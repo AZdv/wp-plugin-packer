ce4b8847e4c35195c13e6d0bc77935fcfead3c73
Asaf Zamir <asaf.zamir@gmail.com>
2015-03-18 21:10:02 +0100

    Removing all public/ files and references

--- trunk/includes/class-wp-plugin-packer.php
+++ trunk/includes/class-wp-plugin-packer.php
@@ -29,7 +29,6 @@ class Wp_Plugin_Packer {
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -68,12 +67,6 @@ class Wp_Plugin_Packer {
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wp-plugin-packer-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wp-plugin-packer-public.php';
-
 		$this->loader = new Wp_Plugin_Packer_Loader();
 
 	}
@@ -109,22 +102,6 @@ class Wp_Plugin_Packer {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Wp_Plugin_Packer_Public( $this->get_wp_plugin_packer(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
